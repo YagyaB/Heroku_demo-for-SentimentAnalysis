fbdebc4823af5419c8c3aa64f8002fd50d22c92a
Yagya Raj Bhatta <54733074+YagyaB@users.noreply.github.com>
2020-12-27 02:19:52 +0545

    Update settings.py

--- SentimentAnalysis3/settings.py
+++ SentimentAnalysis3/settings.py
@@ -119,8 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
-
-STATICFILES_DIRs = [
-    os.path.join(BASE_DIR,'static'),
-]
